7194a587d8294cfb521d4fb0062fb271f60a9c18
Баженов Антон Геннадьевич <BazhenovAG1@sibintek.ru>
2023-10-15 13:03:31 +0300

    test

--- UserService.Infrastructure/Services/TokenGeneratorService.cs
+++ UserService.Infrastructure/Services/TokenGeneratorService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using UserService.Application.Interfaces;
